ffccf662ccbc6d4afa6cea5c054619e03b9591ab
Eduardo Schulz <eduardo.schulz68@gmail.com>
2023-03-14 18:09:16 -0300

    created interface and the insertion of tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import AddTask from "./components/AddTask/AddTask";
+import Tasks from "./components/Tasks/Tasks";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			title: "Estudar Programação",
+			completed: false,
+		},
+		{
+			id: 2,
+			title: "Ler Livros",
+			completed: true,
+		},
+	]);
+
+	const handleTaskClick = (taskId) => {
+		const newTasks = tasks.map((task) => {
+			if (task.id === taskId)
+				return { ...task, completed: !task.completed };
+
+			return task;
+		});
+
+		setTasks(newTasks);
+	};
+
+	const handleTaskAddition = (taskTitle) => {
+		const newTask = [
+			...tasks,
+			{
+				title: taskTitle,
+				id: uuidv4(),
+				completed: false,
+			},
+		];
+
+		setTasks(newTask);
+	};
+
+	return (
+		<>
+			<div className="container">
+				<AddTask handleTaskAddition={handleTaskAddition} />
+				<Tasks tasks={tasks} handleTaskClick={handleTaskClick} />
+			</div>
+		</>
+	);
+};
 
 export default App;
